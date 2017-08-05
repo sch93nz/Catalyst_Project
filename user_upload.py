7d6f6db82d22cdcbce33610f62ed8d420c89f5f1
sch93nz <sch93nz@hotmail.com>
2017-08-05 17:35:48 +1200

    testing different user names and adding comments to the code. Have not tested any of this code one mySQL yet as i am not working at home.

--- user_upload.py
+++ user_upload.py
@@ -3,14 +3,17 @@ import argparse
 import MySQLdb
 
 def create_tables(db):
-    """Creates the tables in the database"""
-    command = 'create table users (name VARCHAR(20),surname'\
-                'VARCHAR(20),email VARChAR(256) UNIQUE);'
+    """Creates the tables in the database then quits
+    If The table already exist my through a error which
+    will be printed to standard out"""
+
+    command = 'CREATE TABLE users ( name VARCHAR(20) , surname'\
+                'VARCHAR(20), email VARCHAR(256) UNIQUE);'
     if not DRY_RUN:
         try:
             db.query(command)
             db.commit()
-        except MySQLdb.Error, error:
+        except MySQLdb.Error, error:# MySql Error handling only
             print str(error)
             quit()
     else:
@@ -24,7 +27,8 @@ def print_invalid_email(first, surname, email):
     print "First= "+first+" surname= "+surname+" email= "+email + " )"
 
 def check_email(email, first, surname):
-    """This will return true if the email conforms to requirements"""
+    """This will return true if the email conforms to requirements
+    As more checks are thought of they will be put here"""
     if email.count("@") != 1 or len(email) > 256 or email.count('..') != 0:
         print_invalid_email(first, surname, email)
         return False
@@ -41,23 +45,28 @@ def main(args, db):
     open_file.readline() # this just spits out the headers
     for line in open_file:
         split = line.split(",")
-        split[0] = split[0].capitalize().strip() # capitalizing the first letter and lower casing the rest
-        split[1] = split[1].capitalize().strip() # capitalizing the first letter and lower casing the rest
-        split[2] = split[2].lower().strip() #making the email lower case
-        check = check_email(split[2], split[0], split[1])
-        if check:
-            value = "insert into users (name , surname, email ) values ( '"  + split[0] + "', '" + split[1] + "', '" + split[2] +"' );"
-        if not DRY_RUN:
-            try:
-                db.query(value)
-                db.commit()
-            except MySQLdb.Error, error:
-                print "First= " + split[0] + " surname= " + split[1] + " email= " + split[2] 
-                print str(error)
+        if len(split[0]) > 0 and len(split[1]) > 0 and len(split[2]) > 2: # So this is here to stop empty fields from being inserted
+            split[0] = split[0].capitalize().strip() # capitalizing the first letter and lower casing the rest
+            split[1] = split[1].capitalize().strip() # capitalizing the first letter and lower casing the rest
+            split[2] = split[2].lower().strip() #making the email lower case
+            check = check_email(split[2], split[0], split[1])
+            if check:
+                value = "INSERT INTO users (name , surname, email ) VALUES ( '"  + split[0] + "', '" + split[1] + "', '" + split[2] +"' );"
+                if not DRY_RUN:
+                    try:
+                        db.query(value)
+                        db.commit()
+                    except MySQLdb.Error, error:# MySql Error handling only
+                        print "First= " + split[0] + " surname= " + split[1] + " email= " + split[2] 
+                        print str(error)
+                else:
+                    print value
         else:
-            print value
-if __name__ == "__main__":
+            print "One of the feilds is to small"
+            print split
 
+if __name__ == "__main__":
+    # argument passing
     PARSER = argparse.ArgumentParser(description='Reads a CSV file and adds it to a database.')
     PARSER.add_argument('--file', type=str, help='This is the name of the CSV to be parsed.'
                         , dest='file')
@@ -76,13 +85,15 @@ if __name__ == "__main__":
 
     ARGUMENTS = PARSER.parse_args()
     global DRY_RUN
+    # I am using a global variable because it is less confusing then
+    # always passing a extra argument to every meathod
     DRY_RUN = ARGUMENTS.dry_run
     DATABASE = None
     if not DRY_RUN:
         try:
-            print "connecting to database"
+            print "connecting to database at host="+ARGUMENTS.host
             DATABASE = MySQLdb.connect(host=ARGUMENTS.host, user=ARGUMENTS.user, passwd=ARGUMENTS.password)
-        except MySQLdb.Error, error:
+        except MySQLdb.Error, error: # MySql Error handling only
             print str(error)
             quit()
     else:
