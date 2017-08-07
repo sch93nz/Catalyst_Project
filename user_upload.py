e5c17ee69002e45898b7352843ad2fb5b2338bc3
Matthew <sch93nz@hotmail.com>
2017-08-07 12:52:38 +1200

    a minor change that i thought of

--- user_upload.py
+++ user_upload.py
@@ -20,10 +20,11 @@ def create_tables(db):
     """
     table_removal = """DROP TABLE IF EXISTS users;"""
     command = """CREATE TABLE users (
-        id MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
+        'id' MEDIUMINT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
         name VARCHAR(20) , 
         surname VARCHAR(20), 
         email VARCHAR(256) UNIQUE
+        PRIMARY KEY('id')
         );"""
     if not DRY_RUN:
         try:
